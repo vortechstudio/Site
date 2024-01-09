31de3792a034c62ea5cbaab5f811509df6881657
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2024-01-09 13:53:15 +0100

    :sparkles: feat: Mettre à jour les vues et le composant Livewire de l'accueil
    - Ajout de nouvelles sections à la page d'accueil dans le fichier `resources/views/livewire/acceuil.blade.php`
    - Ajout de la logique de récupération des articles et des services dans le fichier `app/Livewire/Acceuil.php`

--- app/Livewire/Acceuil.php
+++ app/Livewire/Acceuil.php
@@ -2,11 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Services\VortechAPI\Service;
+use App\Services\VortechAPI\Social\BlogService;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Acceuil extends Component
 {
+    public $articles;
+    public $services;
+
+    public function mount()
+    {
+        $blog = new BlogService();
+        $service = new Service();
+        $this->articles = collect($blog->search(["subcategory" => "news", "published" => true]))->take(7);
+        $this->services = collect($service->all());
+
+    }
     #[Title("Accueil")]
     public function render()
     {
