fa32886e30a825c4bccf970a1c2821e57d718c18
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2024-01-09 14:50:44 +0100

    :sparkles: feat: Ajouter des nouvelles fonctionnalités pour les actualités
    Ces nouvelles fonctionnalités incluent les composants Livewire pour afficher les actualités et la liste des actualités. De plus, les vues correspondantes ont été créées.

---
+++ app/Livewire/News/NewsShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\News;
+
+use Livewire\Component;
+
+class NewsShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.news.news-show');
+    }
+}
