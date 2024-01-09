b79b085e89d6c6e125d66b02fab5dbc43b54bb7c
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2024-01-09 19:24:10 +0100

    feat: Ajout de vérifications de contenu dans plusieurs fichiers
    - Ajout de vérifications de contenu dans les fichiers .env.prod, .env.test, .env.example
    - Ajout de vérifications de contenu dans les fichiers slideshow.blade.php, news-list.blade.php, product-list.blade.php, acceuil.blade.php
    - Si le contenu est vide, un message d'erreur est affiché à l'utilisateur.

--- resources/views/livewire/acceuil.blade.php
+++ resources/views/livewire/acceuil.blade.php
@@ -8,15 +8,20 @@
         </div>
 
         <div class="d-flex flex-wrap gap-6">
-            @foreach($articles as $article)
-            <a href="{{ $article->url_to_blog_article }}" class="card card-flush bgi-no-repeat bgi-size-cover bgi-position-center w-300px h-350px hover-scale" style="background-image: url('{{ $article->image_full }}');">
-                <div class="card-body d-flex flex-column justify-content-end align-items-start text-white">
-                    <span class="fs-9">{{ $article->cercles[0]->name }}</span>
-                    <span class="fs-8">{{ $article->title }}</span>
-                </div>
-                <span class="badge badge-primary position-absolute top-0 end-0 m-5">{{ $article->subcategory_string }}</span>
-            </a>
-            @endforeach
+            @if(count($articles) != 0)
+                @foreach($articles as $article)
+                    <a href="{{ $article->url_to_blog_article }}" class="card card-flush bgi-no-repeat bgi-size-cover bgi-position-center w-300px h-350px hover-scale" style="background-image: url('{{ $article->image_full }}');">
+                        <div class="card-body d-flex flex-column justify-content-end align-items-start text-white">
+                            <span class="fs-9">{{ $article->cercles[0]->name }}</span>
+                            <span class="fs-8">{{ $article->title }}</span>
+                        </div>
+                        <span class="badge badge-primary position-absolute top-0 end-0 m-5">{{ $article->subcategory_string }}</span>
+                    </a>
+                @endforeach
+            @else
+                <x-base.is-null
+                    text="Aucun article n'a été publié pour le moment !" />
+            @endif
         </div>
 
     </div>
@@ -30,27 +35,32 @@
             title="Nos Créations" />
 
         <div class="d-flex flex-wrap justify-content-center align-items-center gap-6 mt-5">
-            @foreach($services as $service)
-                <a class="card card-flush shadow-lg card-bordered border-gray-400 text-black w-500px">
-                    <div class="card-header">
-                        <img src="{{ $service->image_src }}" class="card-img-top hover-scale" alt="">
-                    </div>
-                    <div class="card-body">
-                        <div class="d-flex flex-row justify-content-between align-items-center">
-                            <div class="d-flex flex-row">
-                                <div class="symbol symbol-70px me-3">
-                                    <img src="{{ $service->image_icon }}" alt="">
-                                </div>
-                                <div class="d-flex flex-column">
-                                    <span class="fs-3 mb-4">{{ $service->name }}</span>
-                                    <span class="fs-6">{{ $service->description }}</span>
-                                </div>
-                            </div>
-                            <button class="btn btn-lg rounded-5 btn-outline btn-outline-primary hover-elevate-up">Plus</button>
+            @if(count($services) != 0)
+                @foreach($services as $service)
+                    <a class="card card-flush shadow-lg card-bordered border-gray-400 text-black w-500px">
+                        <div class="card-header">
+                            <img src="{{ $service->image_src }}" class="card-img-top hover-scale" alt="">
                         </div>
-                    </div>
-                </a>
-            @endforeach
+                        <div class="card-body">
+                            <div class="d-flex flex-row justify-content-between align-items-center">
+                                <div class="d-flex flex-row">
+                                    <div class="symbol symbol-70px me-3">
+                                        <img src="{{ $service->image_icon }}" alt="">
+                                    </div>
+                                    <div class="d-flex flex-column">
+                                        <span class="fs-3 mb-4">{{ $service->name }}</span>
+                                        <span class="fs-6">{{ $service->description }}</span>
+                                    </div>
+                                </div>
+                                <button class="btn btn-lg rounded-5 btn-outline btn-outline-primary hover-elevate-up">Plus</button>
+                            </div>
+                        </div>
+                    </a>
+                @endforeach
+            @else
+                <x-base.is-null
+                    text="Aucun service n'a été publié pour le moment !" />
+            @endif
         </div>
     </div>
     <div class="separator border-3 separator-content border-primary my-10">
--- resources/views/livewire/news/news-list.blade.php
+++ resources/views/livewire/news/news-list.blade.php
@@ -1,31 +1,36 @@
 <div class="container mt-10">
-    <div class="row">
-        @foreach($blogs as $blog)
-            <div class="col-sm-12 col-lg-6 mb-10">
-                <a href="{{ $blog->url_to_blog_article }}" class="card shadow-lg text-dark">
-                    <img src="{{ $blog->image_full }}" alt="" class="card-img-top hover-scale">
-                    <div class="card-body">
-                        <div class="d-flex flex-column">
-                            <div class="d-flex flex-row align-items-center mb-3">
-                                <span class="text-gray-700 fw-semibold">{{ $blog->cercles[0]->name }}</span>
-                                <span class="bullet bullet-dot bg-primary mx-2"></span>
-                                <span class="text-muted fs-4">{{ \Carbon\Carbon::createFromTimestamp(strtotime($blog->published_at))->format("d/m/Y") }}</span>
+    @if(count($blogs) !== 0)
+        <div class="row">
+            @foreach($blogs as $blog)
+                <div class="col-sm-12 col-lg-6 mb-10">
+                    <a href="{{ $blog->url_to_blog_article }}" class="card shadow-lg text-dark">
+                        <img src="{{ $blog->image_full }}" alt="" class="card-img-top hover-scale">
+                        <div class="card-body">
+                            <div class="d-flex flex-column">
+                                <div class="d-flex flex-row align-items-center mb-3">
+                                    <span class="text-gray-700 fw-semibold">{{ $blog->cercles[0]->name }}</span>
+                                    <span class="bullet bullet-dot bg-primary mx-2"></span>
+                                    <span class="text-muted fs-4">{{ \Carbon\Carbon::createFromTimestamp(strtotime($blog->published_at))->format("d/m/Y") }}</span>
+                                </div>
+                                <span class="fs-3">{{ $blog->title }}</span>
                             </div>
-                            <span class="fs-3">{{ $blog->title }}</span>
                         </div>
-                    </div>
-                </a>
-            </div>
-        @endforeach
-    </div>
-    @if($blogsI->count() > $this->limit)
-        <div class="d-flex flex-center">
-            <button wire:click="loadMore" class="btn btn-lg rounded-5 btn-outline btn-outline-primary w-25" wire:loading.attr="disabled">
-                <span wire:loading.class="d-none">PLUS</span>
-                <span class="d-none" wire:loading.class.remove="d-none">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+        @if($blogsI->count() > $this->limit)
+            <div class="d-flex flex-center">
+                <button wire:click="loadMore" class="btn btn-lg rounded-5 btn-outline btn-outline-primary w-25" wire:loading.attr="disabled">
+                    <span wire:loading.class="d-none">PLUS</span>
+                    <span class="d-none" wire:loading.class.remove="d-none">
                 Chargement... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
             </span>
-            </button>
-        </div>
+                </button>
+            </div>
+        @endif
+    @else
+        <x-base.is-null
+            text="Aucun article de blog n'a été publié pour le moment." />
     @endif
 </div>
